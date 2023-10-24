bec16c1418d4fb6cc02cd38303ca3012bdbf5b84
harris <msca5238@gmail.com>
2023-10-24 19:12:03 +0800

    add chapter04

---
+++ 父与子的编程之旅/动手试一试04.py
@@ -0,0 +1,14 @@
+# 使用float() 由字符串‘12.34’创建一个数字
+num1 = '12.34'
+num2 = float(num1)
+print(num2, type(num2))
+
+# 使用int() 由小数56.78创建一个整数
+num3 = 56.78
+num4 = int(num3)
+print(num4)
+
+# 使用int() 由一个字符串创建一个整数
+num5 = '12'
+num6 = int(num5)
+print(num6, type(num6))
