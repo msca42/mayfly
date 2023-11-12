a2e2cb53c13c4248aeb9427bd98efc95f4f4c013
harris <msca5238@gmail.com>
2023-11-12 14:57:54 +0800

    remove 与孩子学编程

--- 父与子的编程之旅/Listing_1-1.py
+++
@@ -1,5 +0,0 @@
-# 打印字符串
-print("I love pizza")
-print("pizza " * 20)
-print("yum " * 40)
-print("Buuuuurp!")
--- 父与子的编程之旅/动手试一试01.py
+++
@@ -1,7 +0,0 @@
-# 在交互模式中使用Python计算一周有多少分钟
-print(1 * 7 * 24 * 60)
-
-# 打印3行，姓名、出生日期，喜欢颜色
-print("My name is Warren Sande.")
-print("I was born January 1, 1970.")
-print("My favorite color is blue.")
