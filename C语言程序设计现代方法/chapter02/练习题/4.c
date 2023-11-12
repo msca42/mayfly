9c0f6f70601152ecb8e6bfd5cbd85c77c9ee3615
harris <msca5238@gmail.com>
2023-11-12 10:52:02 +0800

    add chapter02

---
+++ C语言程序设计现代方法/chapter02/练习题/4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int  a;
+  int  b;
+  int  c;
+  float i;
+  float j;
+  float k;
+  printf("a = %d\n",a);
+  printf("b = %d\n",b);
+  printf("c = %d\n",c);
+  printf("i = %f\n",i);
+  printf("j = %f\n",j);
+  printf("k = %f\n",k);
+
+}
