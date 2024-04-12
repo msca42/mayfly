004abf157685e42f113cb4d7fce1038ed1b62878
harris <msca5238@gmail.com>
2024-04-12 21:14:01 +0800

    add chapter05

---
+++ Python编程快速上手/chapter05/实践项目/3.py
@@ -0,0 +1,11 @@
+from inventory import displayInventory
+
+
+def addToInventory(inventory, addedItems):
+  return 0
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
