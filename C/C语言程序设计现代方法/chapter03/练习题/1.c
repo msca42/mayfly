c61dc89698606338f086f483f1c9f09194b91fa5
harris <msca5238@gmail.com>
2024-06-05 16:19:09 +0800

    todo add  chapter03

---
+++ C/C语言程序设计现代方法/chapter03/练习题/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // a:    86,1040
+  printf("%6d,%4d", 86, 1040);
+  // b:3.0250e+01  ,
+  printf("%12.5e", 30.253);
+  // c: 
+  printf("%.4f",83.162);
+}
