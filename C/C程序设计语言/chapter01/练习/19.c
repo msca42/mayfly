a19efaf80d5674238195db0da592ba14245587ff
harris <msca5238@gmail.com>
2024-06-07 13:28:36 +0800

    add 引言

---
+++ C/C程序设计语言/chapter01/练习/19.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
+void reverse(char s[]);
+
+int main(void)
+{
+  char line[MAXLINE];
+  while (getline(line, MAXLINE) > 0)
+  {
+    reverse(line);
+    printf("%s", line);
+  }
+}
+
+void reverse(char s[])
+{
+  int i, j;
+  char temp;
+
+  i = 0;
+  while (s[i] != '\0')
+  {
+    ++i;
+  }
+  --i;
+  if (s[i] == '\n')
+    --i;
+  j = 0;
+  while (j < i)
+  {
+    temp = s[j];
+    s[j] = s[i];
+    s[i] = temp;
+    --i;
+    ++j;
+  }
+}
+
+int getline(char s[], int lim)
+{
+  int c, i, j;
+
+  j = 0;
+  for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+  {
+    if (i < lim - 2)
+    {
+      s[j] = c;
+      ++j;
+    }
+
+    if (c == '\n')
+    {
+      s[j] = c;
+      ++j;
+      ++i;
+    }
+    s[i] = '\0';
+    return i;
+  }
+}
