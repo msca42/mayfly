4060a736a76a6878370974da055e07075848fc0a
harris <msca5238@gmail.com>
2024-06-11 22:16:07 +0800

    暂存chapter03

---
+++ C/C程序设计语言/chapter03/switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int c, i, nwhite, nother, ndigit[10];
+
+  nwhite = nother = 0;
+  for (i = 0; i < 10; i++)
+    ndigit[i] = 0;
+  while ((c = getchar()) != EOF)
+  {
+    switch (c)
+    {
+    case '0':
+    
+      /* code */
+      break;
+    
+    default:
+      break;
+    }
+  }
+}
