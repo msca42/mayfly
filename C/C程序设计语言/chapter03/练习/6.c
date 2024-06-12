b78ad45bc016a608fcd2caa5717d8a07fabc1981
harris <msca5238@gmail.com>
2024-06-12 14:52:06 +0800

    add chapter03

---
+++ C/C程序设计语言/chapter03/练习/6.c
@@ -0,0 +1,24 @@
+#define abs(x) ((x) < 0 ? -(x) : (x))
+
+void itoa(int n, char s[], int w)
+{
+  int i, sign;
+  void reverse(char s[]);
+
+  sign = n;
+  i = 0;
+  do
+  {
+    s[i++] = abs(n % 10) + '0';
+  } while ((n /= 10) != 0);
+
+  if (sign < 0)
+    s[i++] = '-';
+  while (i < w)
+  {
+    s[i++] = ' ';
+  }
+
+  s[i] = '\0';
+  reverse(s);
+}
