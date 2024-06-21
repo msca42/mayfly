07fbabe8613b3d7a245af40c1b4ebab72cc0c33b
harris <msca5238@gmail.com>
2024-06-21 15:48:11 +0800

    add chapteer07

---
+++ C/C语言程序设计现代方法/chapter07/练习题/3.c
@@ -0,0 +1,10 @@
+/*
+合法
+long double
+unsigned long
+
+非法
+short unsigned int / unsigned short int
+short float / float
+
+*/
---
+++ C/C语言程序设计现代方法/chapter07/练习题/4.c
@@ -0,0 +1,9 @@
+/*
+合法
+i += c;
+c = 2 * c  - 1;
+putchar(c)
+
+非法
+printf(c)
+*/
