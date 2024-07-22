7887d00b80ede0e26861b27c4de16288ae29f007
harris <msca5238@gmail.com>
2024-07-22 09:06:55 +0800

    remove 廖雪峰python

--- Python/廖雪峰/01_第一个Python程序/hello.py
+++
@@ -1,4 +0,0 @@
-# 直接运行Python程序步骤
-# 注释 编译器路径 chmod可执行权限
-#!/usr/bin/env python3
-print('hello,world')
--- Python/廖雪峰/01_第一个Python程序/hello2.py
+++
@@ -1,3 +0,0 @@
-# 输入函数input() 输出函数print()
-name = input('please enter your name: ')
-print('hello,', name)
