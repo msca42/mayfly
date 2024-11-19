5d6bd651a9becdca05417edd1df46cbcfa55988e
harris <msca5238@gmail.com>
2024-11-19 18:40:14 +0800

    暂存

---
+++ C/C语言程序设计现代方法/基本特性/chapter02/pun.c
@@ -0,0 +1,13 @@
+/* Name: pun.c                */
+/* Purpose: Prints a bad pun. */
+/* Author: K. N. King         */
+
+#include <stdio.h> // 载入标准库
+
+int main(void) // 主程序 int表示返回一个整数状态码，void表示没有参数
+{
+    // 格式化标准输出函数 \n 表示换行
+    printf("To C, or not to C: that is the question.\n");
+    // 程序终止系统返回值
+    return 0;
+}
