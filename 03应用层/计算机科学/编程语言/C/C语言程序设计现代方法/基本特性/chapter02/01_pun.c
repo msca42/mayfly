102fc6290d7552fe12bee6184b6a27e97ab7f8eb
mayfly <msca5238@gmail.com>
2025-05-08 15:43:25 +0800

    暂存

---
+++ 03应用层/计算机科学/编程语言/C/C语言程序设计现代方法/基本特性/chapter02/01_pun.c
@@ -0,0 +1,11 @@
+// 包含C语言标准输入/输出库的信息
+#include <stdio.h>
+
+int main(void)
+{
+    // 可执行代码包裹在main函数内
+    // 格式化输出 \n表示换行
+    printf("To C, or not to C: that is the question.\n");
+    // 程序终止时会向操作系统返回值0
+    return 0;
+}
